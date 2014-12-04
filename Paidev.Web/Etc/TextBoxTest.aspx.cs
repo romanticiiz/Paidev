10f9d24edf8bea963ec313c53ee2f788496db752
LSangHyun <romanticiiz@naver.com>
2014-12-04 14:34:10 +0900

    141124_2
    141124_2

---
+++ Paidev.Web/Etc/TextBoxTest.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Paidev.Web.Etc
+{
+    public partial class TextBoxTest : System.Web.UI.Page
+    {
+        private string tmpFloatNum = string.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string num = "5";
+
+            double tmpPrice = (Convert.ToInt32(num) - 1) + 0.99;            
+
+            return;
+        }
+    }
+}

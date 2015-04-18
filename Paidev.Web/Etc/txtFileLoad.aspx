d2a6aa53d94ce3e79d4738a0ca50dce19109050b
romanticiiz <romanticiiz@naver.com>
2015-04-18 22:28:46 +0900

    Txt파일 업로드 및 불러오기 관련 예제 작업

---
+++ Paidev.Web/Etc/txtFileLoad.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="txtFileLoad.aspx.cs" Inherits="Paidev.Web.Etc.txtFileLoad" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script type="text/javascript" src="/Js/jquery-1.9.0.js"></script>
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+
+            $("#btnConfirm").click(function () {
+                var file = "/Upload_img/" + "<%=fileName %>";
+                
+                $.ajax({
+                    url: file,
+                    dataType: "text",
+                    success: function (data) {
+                        $("#target").html(data);
+                    }
+                });
+            });
+
+            $("#<%=FileUpload.ClientID %>").change(function () {
+                $("#<%=btnFileUpload.ClientID %>").trigger("click");
+            });
+
+        });
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload" runat="server" />
+        <button type="button" name="btnConfirm" id="btnConfirm">확인</button>
+        <p id="pTag"></p>
+        <div id="target"></div>
+        <asp:Button runat="server" ID="btnFileUpload" OnClick="btnFileUpload_Button" />        
+    </div>
+    </form>
+</body>
+</html>
---
+++ Paidev.Web/Etc/txtFileLoad.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+
+namespace Paidev.Web.Etc
+{
+    public partial class txtFileLoad : System.Web.UI.Page
+    {
+        protected string fileName = string.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            fileName = string.IsNullOrEmpty(Request["fileName"]) ? "" : Convert.ToString(Request["fileName"]);
+        }
+
+        protected void btnFileUpload_Button(object sender, EventArgs e)
+        {
+            DirectoryInfo aDir = new DirectoryInfo(Server.MapPath("/Upload_img/"));
+            FileInfo[] files;
+
+            if (aDir != null)
+            {
+                DirectoryInfo[] directories = aDir.GetDirectories();
+
+                for (int i = 0; i < directories.Length; i++)
+                {
+                    files = directories[i].GetFiles();
+                    for (int j = files.Length - 1; j >= 0; j--)
+                    {
+                        files[j].Delete();
+                    }
+                }
+
+                files = aDir.GetFiles();
+
+                for (int i = files.Length - 1; i >= 0; i--)
+                {
+                    files[i].Delete();
+                }
+            }            
+
+            if (FileUpload.HasFile)
+            {
+                string fileName = Path.Combine(Server.MapPath("/Upload_img/"), FileUpload.FileName);
+                FileUpload.SaveAs(fileName);
+
+                Response.Redirect("/etc/txtFileLoad.aspx?fileName="+ FileUpload.FileName.ToString() +"");
+            }
+
+            //string virturePath = Request.QueryString["fileT"];
+            //string realPath = Server.MapPath("/Upload_img/" + virturePath);
+
+            //int length = Request.ContentLength;
+            //byte[] bytes = new byte[length];
+            //Request.InputStream.Read(bytes, 0, length);
+
+            //string fileName = Request.Headers["file-name"];
+            //string fileSize = Request.Headers["file-size"];
+            //string fileType = Request.Headers["file-type"];
+
+            //string fileUrl = Server.MapPath("/Upload_img/");
+        }
+    }
+}

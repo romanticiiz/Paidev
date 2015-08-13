7a773d138974810c1e16233e93d2ffc960f4b0d7
unknown <romanticiiz@ROMANTICIIZ-PC.nexon.corp>
2015-08-13 13:57:01 +0900

    동기화

--- Paidev.Web/Js/fnObj.js
+++ Paidev.Web/Js/fnObj.js
@@ -1,64 +1,79 @@
-﻿var fnObj = fnObj || {};
-fnObj.ajax = {
-    history: {
-        init: function() {
-            var a = $('#ajaxContents').find(".boardView").length ? "view" : "list";
+﻿var fnObj = {};
+fnObj.val = {    
+    BoardInfo: function(boardSN, articleSN) {
+        var n4BoardSN = 0;
 
-            fnObj.ajax.history.replace({
-                type: a
-                , url: document.URL
-            })
-            , window.onpopstate = function (e) {
-                e.state && (fnObj.ajax.load({
-                    type: e.state.type,
-                    url: e.state.url,
-                    popstate: !0
-                }))
-            }
+        setValue = function (boardSN) {
+            n4BoardSN = boardSN
         }
-        , push: function (obj) {
-            history.pushState({
-                type: obj.type
-                , url: obj.url                
-            }, "", obj.url)
-        }
-        , replace: function (obj) {
-            history.replaceState({
-                type: obj.type
-                , url: obj.url
-            }, "", obj.url)
-        }
-    }
-    , load: function (obj) {
-        $.ajax({
-            type: "POST"
-            , url: obj.url
-            , success: function (data) {
-                var tmp = $("<html />").html(data);
-                
-                // history
-                !obj.popstate && fnObj.ajax.history.push({                    
-                    type: obj.type,
-                    url: obj.url,                    
-                });
 
-                var contents = tmp.find('#contents');
-
-                $('#contents').hide();
-                $('#ajaxContents').html(contents);
-            }
-        });
+        getValue = function () {
+            return n4BoardSN;
+        }
     }
 }
 
-$(document).ready(function () {
-    // historyInit
-    fnObj.ajax.history.init();
+//var fnObj = fnObj || {};
+//fnObj.ajax = {
+//    history: {
+//        init: function() {
+//            var a = $('#ajaxContents').find(".boardView").length ? "view" : "list";
 
-    $(document).on('click', 'a[data-ajax-board]', function (e) {
-        fnObj.ajax.load({
-            type: $(this).attr("data-ajax-board")
-            , url: $(this).attr("href")
-        }), e.preventDefault()
-    });
-});
+//            fnObj.ajax.history.replace({
+//                type: a
+//                , url: document.URL
+//            })
+//            , window.onpopstate = function (e) {
+//                e.state && (fnObj.ajax.load({
+//                    type: e.state.type,
+//                    url: e.state.url,
+//                    popstate: !0
+//                }))
+//            }
+//        }
+//        , push: function (obj) {
+//            history.pushState({
+//                type: obj.type
+//                , url: obj.url                
+//            }, "", obj.url)
+//        }
+//        , replace: function (obj) {
+//            history.replaceState({
+//                type: obj.type
+//                , url: obj.url
+//            }, "", obj.url)
+//        }
+//    }
+//    , load: function (obj) {
+//        $.ajax({
+//            type: "POST"
+//            , url: obj.url
+//            , success: function (data) {
+//                var tmp = $("<html />").html(data);
+                
+//                // history
+//                !obj.popstate && fnObj.ajax.history.push({                    
+//                    type: obj.type,
+//                    url: obj.url,                    
+//                });
+
+//                var contents = tmp.find('#contents');
+
+//                $('#contents').hide();
+//                $('#ajaxContents').html(contents);
+//            }
+//        });
+//    }
+//}
+
+//$(document).ready(function () {
+//    // historyInit
+//    fnObj.ajax.history.init();
+
+//    $(document).on('click', 'a[data-ajax-board]', function (e) {
+//        fnObj.ajax.load({
+//            type: $(this).attr("data-ajax-board")
+//            , url: $(this).attr("href")
+//        }), e.preventDefault()
+//    });
+//});
